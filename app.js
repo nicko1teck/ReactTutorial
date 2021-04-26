3483337b9d39b25f0c36d0c0f629b7b6019838bc
Nicko <nicko1teck@gmail.com>
2021-04-26 16:54:09 -0500

    folder restructure for heroku deploy

---
+++ app.js
@@ -0,0 +1,97 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var cors = require('cors');
+const router = express.Router();
+
+var app = express();
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var booksRouter = require('./routes/books');
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(cors());
+
+/*
+app.use(function(req,res,next) {
+  setTimeout(next,2000);
+})
+
+app.use(function(req,res,next) {
+
+  if (req.url.match(/\d+/)) {
+    res.sendStatus(500);
+  }
+  else {
+    next();
+  }
+    
+})
+*/
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/books', booksRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500).json({ 'error': err.toString() });
+});
+
+// ** MIDDLEWARE ** //
+const whitelist = ['http://localhost:3000', 'http://localhost:8000', 'https://bibliotech-react.herokuapp.com/'];
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log("** Origin of request " + origin)
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      console.log("Origin acceptable")
+      callback(null, true)
+    } else {
+      console.log("Origin rejected")
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
+
+app.use(cors(corsOptions))
+
+
+
+
+
+// Serve client react instead of backend 
+// Add the follwing code to your server file on the backend 
+//const path = require('path');
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+// Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
+
+
+
+
+
+
+module.exports = app;
---
+++ routes/books.js
@@ -0,0 +1,97 @@
+const express = require('express');
+const mysql = require('mysql');
+
+const router = express.Router();
+//const connectionPool = require('/database/connection-pool.js');
+//const BookRepository = require('/database/book-repository.js');
+// Relative path example:  ReactTutorial\database\book-repository.js
+const connectionPool = require('../database/connection-pool');
+const BookRepository = require('../database/book-repository');
+
+let repository = new BookRepository(connectionPool);
+
+
+router.get('/:id', function (req, res) {
+  repository.get(req.params.id, (err, result) => {
+    if (err) {
+      res.status(500).json({ 'error': err.toString() });
+    }
+    else {
+      res.status(200).json(result);
+    }
+  })
+})
+
+
+
+
+// Get a single book
+router.get('/:id', function (req, res) {
+  repository.get(req.params.id, (err, result) => {
+    if (err) {
+      res.status(500).json({ 'error': err.toString() });
+    }
+    else {
+      res.status(200).json(result);
+    }
+  });
+})
+
+
+
+// Update a book
+router.put('/:id', function (req, res) {
+  repository.update(req.params.id, req.body, (err, result) => {
+    if (err) {
+      res.status(500).json({ 'error': err.toString() });
+    }
+    else {
+      res.sendStatus(200);
+    }
+  });
+})
+
+
+
+// Delete a book
+router.delete('/:id', function (req, res) {
+  repository.delete(req.params.id, (err, result) => {
+    if (err) {
+      res.status(500).json({ 'error': err.toString() });
+    }
+    else {
+      res.sendStatus(200);
+    }
+  });
+})
+
+
+
+// Save a book
+router.post('/', function (req, res) {
+
+  repository.save(req.body, (err, result) => {
+    if (err) {
+      console.log("THERE WAS AN ERROR IN THE SAVE METHOD");
+      res.status(500).json({ 'error': err.toString() });
+    }
+    else {
+      res.sendStatus(200);
+    }
+  });
+})
+
+// Get all the books
+router.get('/', function (req, res) {
+  repository.getAll((err, result) => {
+    if (err) {
+      res.status(500).json({ 'error': err.toString() });
+    }
+    else {
+      res.status(200).json(result);
+    }
+  });
+});
+
+
+module.exports = router;

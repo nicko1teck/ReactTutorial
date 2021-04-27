d10cd997d4afb67fdb0bbabd7195e0b40e95ac29
Nicko <nicko1teck@gmail.com>
2021-04-26 19:56:42 -0500

    https://www.youtube.com/watch?v=5PaUiPyBDJY&t=99s

--- app.js
+++ app.js
@@ -81,8 +81,8 @@ app.use(cors(corsOptions))
 //const path = require('path');
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
-  //app.use(express.static(path.join(__dirname, 'client/build')));
-  app.use(express.static('client/build'));
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  //app.use(express.static('client/build'));
 // Handle React routing, return all requests to React app
   app.get('*', function(req, res) {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));

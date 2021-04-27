0e734d766dda3bf8e4d5eaca9757833054483be3
Nicko <nicko1teck@gmail.com>
2021-04-26 19:11:38 -0500

    https://www.youtube.com/watch?v=5PaUiPyBDJY&t=99s

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@
 const db = {
     'user': 'en39ofgw7mc2r1pp',
     'password': 'nzd9tybiwsotft43',
-    'host': 'pfw0ltdr46khxib3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    'host': process.env.JAWSDB_URL || 'pfw0ltdr46khxib3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
     'database': 't6pd1yyl4reebhx9',
     'timezone': 'utc', 
 }
